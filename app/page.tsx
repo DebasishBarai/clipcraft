01ec2c4d4063ac926024416a1f648306cb1db70d
Debasish Barai <debasishbaraiju@gmail.com>
2023-09-16 11:08:31 +0000

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Page = () => {
   return(
-    <main className="min-w-full min-h-screen flex flex-col justify-center items-center">Welcome to the Home page</main>
+    <main className='min-w-full min-h-screen flex flex-col'>
+      <div className='flex flex-row justify-end items-end'>
+        <button className='bg-blue-800 border border-red-700 border-solid'>SignIn</button>
+      </div>
+      <h1>Welcome to the Home page</h1></main>
   )
 }
 
