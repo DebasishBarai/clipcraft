c3b466299abd7f94b90ffeee44e59aa22f042277
Debasish Barai <debasishbaraiju@gmail.com>
2023-09-17 11:36:02 +0000

    small changes

--- app/components/topbar.tsx
+++ app/components/topbar.tsx
@@ -3,7 +3,8 @@ import mode_svg from '../../public/mode.svg'
 
 const TopBar = () => {
     return (
-        <div className='h-fit flex flex-row justify-end'>
+        <div className='h-fit flex flex-row justify-end items-center'>
+            <button className='bg-blue-900 p-4 text-xl text-white rounded-xl'>SignIn</button>
             <Image src = {mode_svg} alt = 'toggle mode'/>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 const Page = () => {
   return(
-    <main className='min-w-full min-h-screen flex flex-col'>
-  <div className='flex flex-row justify-end flex-grow-0'>
-    <button className='bg-blue-900 p-4 text-xl text-white rounded-xl m-4'>SignIn</button>
-  </div>
-  <div className='flex flex-col justify-center items-center flex-grow text-4xl text-blue-400'>
+    <main className='min-w-full min-h-screen justify-center items-center flex-grow text-4xl text-blue-400'>
     <h1>Welcome to <span className='text-blue-800 font-bold'>clipcraft</span> app</h1>
-  </div>
 </main>
   )
 }
